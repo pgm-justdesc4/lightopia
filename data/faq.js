d76fe5a6ed470ce0fc6b6211dffba96b543852b7
pgm-justdesc4 <justin.descan@student.arteveldehs.be>
2023-11-21 16:22:47 +0100

    Added header, navigation and video

---
+++ data/faq.js
@@ -0,0 +1,16 @@
+const questions = [
+  {
+    category: "Algemeen",
+    question: "Wat is Lightopia Brussels?",
+    answer:
+      "Een meermaals georganiseerd lantaarn- en lichtfestival dat traditionele handgemaakte lantaarns combineert met modern design en technologie zoals een watershow, 3D mapping en artistieke installaties om een immersieve ervaring te creëren.",
+  },
+  {
+    category: "Algemeen",
+    question: "Waar vindt Lightopia Brussels plaats?",
+    answer: `Lightopia Brussels vindt plaats in het kasteel van Groot-Bijgaarden.
+
+      Isidoor van Beverenstraat 5
+      1702 Groot-Bijgaarden`,
+  },
+];
