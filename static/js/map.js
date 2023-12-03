d29c53521ea26c2b4eddcb2e000db394b1c30e47
pgm-justdesc4 <justin.descan@student.arteveldehs.be>
2023-12-03 22:19:09 +0100

    added map-pointers styling and js

--- static/js/map.js
+++ static/js/map.js
@@ -1,46 +1,47 @@
 (function () {
   const $map = document.getElementById("map");
-  const $pointers = document.getElementById("map-pointers");
-  // const $pointer = document.getElementById("pointer");
 
   function getHTMLForPointers(pointers) {
-    let html = `<img src="static/img/images/trail-map.jpg" alt="Trail-map">`;
+    let html = `<img src="static/img/images/trail-map.jpg" alt="Trail-map">
+                <div id="pointers">`;
     pointers.forEach((pointer) => {
       html += `
-        <figure id="pointer" class"map-pointers" 
+        <figure data-id="${pointer.id}" class="map-pointers" 
             style="
-                width: 3rem;
-                background-color: hsl(149, 76%, 46%);
-                position: absolute; 
+                position: absolute;
                 top:${pointer.y}; 
-                left:${pointer.x};
-                cursor: pointer;
-                padding: .75rem;
-                border-radius: inherit;
-                border-radius: 50%;">
-            <img src="${pointer.icon}" alt="${pointer.pointer}">
+                left:${pointer.x};">
+            <img src="${pointer.icon}" alt="${pointer.id}">
         </figure>`;
     });
+    html += `
+    </div>
+    <div id="imageWrapper">
+    </div>`;
     return html;
   }
 
   function registerListeners() {
-    /*
-    $pointer.forEach((pointer) => {
-      pointer.addEventListener("click", function () {
-        const $mapWrapper = document.getElementById("map-wrapper");
-        let html = "";
-        html += `
-            <div id="closeImage" class="galery-wrapper">
-                <figure id="closeBtnImage" class="close-btn">
-                    <img src="static/img/images/close-btn.svg" alt="Close button">
-                </figure>
-                <figure>
-                    <img src="${pointer.image}" alt="${pointer.pointer}">
-                </figure>
-            </div>`;
+    const $pointers = document.querySelectorAll("#pointers figure");
+    $pointers.forEach((pointer) => {
+      pointer.addEventListener("click", function (e) {
+        const id = e.currentTarget.dataset.id;
+        const pointer = pointers.find((pointer) => {
+          return pointer.id === id;
+        });
 
-        $mapWrapper.innerHTML = html;
+        let html = `
+        <div id="closeImage" class="map__wrapper">
+        <figure id="closeBtnImage" class="close-btn">
+            <img src="static/img/images/close-btn.svg" alt="Close button">
+        </figure>
+        <figure>
+            <img src="${pointer.image}" alt="${pointer.id}">
+        </figure>
+        </div>`;
+
+        const $imageWrapper = document.getElementById("imageWrapper");
+        $imageWrapper.innerHTML = html;
 
         const $closeBtnImage = document.getElementById("closeBtnImage");
         $closeBtnImage.addEventListener("click", function () {
@@ -48,16 +49,16 @@
           $closeImage.classList.add("close");
         });
       });
-    });*/
+    });
   }
 
   function buildUI() {
-    $pointers.innerHTML = getHTMLForPointers(pointers);
+    $map.innerHTML = getHTMLForPointers(pointers);
   }
 
   function initialize() {
-    registerListeners();
     buildUI();
+    registerListeners();
   }
 
   initialize();
